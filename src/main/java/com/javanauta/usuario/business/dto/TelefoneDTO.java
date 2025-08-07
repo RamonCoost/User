9dd231a4601ca6db945713cf8df85cceb5259387
RamonCoost <ramoncoost@gmail.com>
2025-08-07 20:47:23 -0300

    adicionado método que salva dados  usuario

---
+++ src/main/java/com/javanauta/usuario/business/dto/TelefoneDTO.java
@@ -0,0 +1,14 @@
+package com.javanauta.usuario.business.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TelefoneDTO {
+
+    private String numero;
+    private String ddd;
+}
